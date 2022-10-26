c51b94f10606c5e598baeecb0da38d73b67de658
Chez123321 <76134402+Chez123321@users.noreply.github.com>
2022-10-25 22:07:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+window.addEventListener("keyup", run)
+var text = document.getElementById("text")
+var scales = ["D", "G", "C", "F", "Bb"]
+
+function run() {
+    text.innerHTML = scales[Math.floor(Math.random() * scales.length)]
+}
 
